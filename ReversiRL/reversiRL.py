bb8efbe67e471d31a0fade92d079098a45ea2c57
Aubrey Choi <maitte@naver.com>
2021-10-01 18:04:54 +0900

    RL programming

--- ReversiRL/reversiRL.py
+++ ReversiRL/reversiRL.py
@@ -1,6 +1,21 @@
 import socket
 import random
 import keyboard
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from collections import deque
+
+# parameters
+alpha = 0.001
+gamma = 0.95
+epsilon = 1
+epsilon_min = 0.001
+epsilon_decay = 0.999
+batch_size = 24
+
+# memory
+memory = deque([], maxlen=500)
 
 reads = { "S" : 1, "T" : 64, "Q" : 4, "A" : 0 }
 def recv(sock):
@@ -24,14 +39,52 @@ def recv(sock):
 def OnQuit(buf):
     w = int(buf[:2])
     b = int(buf[2:])
+    result = w-b if turn == 1 else b-w
     print(f"W: {w}, B: {b}")
-    winText = ("You Lose!!", "You Win!!", "Tie")
-    win = 2 if w == b else turn == 1 if w > b else turn == 2
+    winText = ("You Lose!!", "Tie", "You Win!!")
+    win = 1 if result == 0 else 2 if result > 0 else 0
     print(winText[win])
-    return win
-    
+    return win, result
+
+def BuildModel():
+	model = keras.Sequential([
+		keras.layers.Dense(64, activation="relu"),
+		keras.layers.Dense(64, activation="relu"),
+		keras.layers.Dense(64, activation="linear")
+	])
+	model.compile(loss="mean_squared_error",
+		optimizer=keras.optimizers.Adam(learning_rate=alpha))
+	return model
+	
+def Action(model, board, turn):
+    hints = []
+    state = np.zeros(64);
+    for i in range(64):
+        c = int(buf[i])
+        if c == 0: 
+            state[i] = -1.0
+            hints.append(i)
+        elif c == 3: state[i] = -1.0
+        elif c == turn: state[i] = 1.0
+        else: state[i] = 0.0
+        predict = model.predict(state)
+        if np.random.rand() <= epsilon:
+            return state, predict, hints[random.randrange(len(hints))]
+        return state, predict, np.argmax(predict[board==-1])
+
+def Replay(model):
+    minibatch = random.sample(memory, batch_size)
+    x, y = [], []
+    for state, predict, action, reward in minibatch:
+        x.append(state)
+        predict[action] = reward
+        y.append(predict)
+    model.fit(x, y)
+    if epsilon > epsilon_min: epsilon *= epsilon_decay
+	
 quitFlag = False
 winlose = [0, 0, 0]
+model = BuildModel()
 
 while not quitFlag:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,6 +98,7 @@ while not quitFlag:
     print(f"Game {winlose[0]+winlose[1]+winlose[2]+1} Turn: {colors[turn]}")
     if cmd == "E": break
 
+    episode = []
     while True:
         if keyboard.is_pressed("q"): quitFlag = True
         cmd, buf = recv(sock)
@@ -52,21 +106,24 @@ while not quitFlag:
             print(f"Network Error!! {buf}")
             break
         if cmd == "Q":
-            w = OnQuit(buf)
+            w, r = OnQuit(buf)
             winlose[w] += 1
+            reward = w*1000 + r*100
+            for e in episode[::-1]:
+                memory.append((e[0], e[1], e[2], reward))
+                reward *= gamma
             break
         if cmd == "A":
             print("Game Abort!!")
             break
-        hints = []
-        board = []
-        for i in range(64):
-            board.append(int(buf[i]))
-            if board[-1] == 0:
-                hints.append(i)
-        p = random.randrange(len(hints))
-        sock.send(("P%02d"%hints[p]).encode("ascii"))
+        if cmd != "T":
+            print(f"Unknown command {cmd}")
+            break
+        state, predict, p = Action(model, buf, turn)
+        sock.send(("P%02d"%p).encode("ascii"))
+        episode.append((state, predict, p))
 
     sock.close()
+    Replay(model)
 
 print(f"Wins: {winlose[1]}, Loses: {winlose[0]}")
